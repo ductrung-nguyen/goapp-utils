7c575dd67b0c38f4801bbff3e70ff75e03c60a40
Duc Trung NGUYEN <ductrung.nguyen@amadeus.com>
2022-12-20 13:27:29 +0100

    feat: add function to get k8s config

--- pkg/utils/k8sclient.go
+++ pkg/utils/k8sclient.go
@@ -15,18 +15,19 @@ type K8sClient struct {
 // Using this way, we can easily create a mock object that satisfies this interface
 type IK8sClientHelper interface {
 	GetClient(pathToCfg string) (*K8sClient, error)
+	GetClientAndConfig(pathToCfg string) (*K8sClient, *rest.Config, error)
 }
 
 // ClienHelper is a helper class that implement IClientHelper, and returns the real Kubernetes Clientset
 type K8sClientHelper struct {
 }
 
-// GetClient returns a Kubernetes Clientset which is built from a given config file path,
+// GetClientAndConfig returns a Kubernetes Clientset and the REST configuration which is built from a given config file path,
 // for example `~/.kube/config`
 // if the file path is empty, we will use the mode "InCluster"
 // (with a token of serviec account stored in `/var/run/secrets/kubernetes.io/serviceaccount/token`)
 // otherwise, use the server information and authentication information from the path
-func (c K8sClientHelper) GetClient(pathToCfg string) (*K8sClient, error) {
+func (c K8sClientHelper) GetClientAndConfig(pathToCfg string) (*K8sClient, *rest.Config, error) {
 	var config *rest.Config
 	var err error
 	if pathToCfg == "" {
@@ -37,12 +38,22 @@ func (c K8sClientHelper) GetClient(pathToCfg string) (*K8sClient, error) {
 		config, err = clientcmd.BuildConfigFromFlags("", pathToCfg)
 	}
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	if clientset, err := kubernetes.NewForConfig(config); err != nil {
-		return nil, err
+		return nil, nil, err
 	} else {
-		return &K8sClient{Clientset: clientset}, nil
+		return &K8sClient{Clientset: clientset}, config, nil
 	}
+}
+
+// GetClient returns a Kubernetes Clientset which is built from a given config file path,
+// for example `~/.kube/config`
+// if the file path is empty, we will use the mode "InCluster"
+// (with a token of serviec account stored in `/var/run/secrets/kubernetes.io/serviceaccount/token`)
+// otherwise, use the server information and authentication information from the path
+func (c K8sClientHelper) GetClient(pathToCfg string) (*K8sClient, error) {
+	client, _, err := c.GetClientAndConfig(pathToCfg)
+	return client, err
 }
