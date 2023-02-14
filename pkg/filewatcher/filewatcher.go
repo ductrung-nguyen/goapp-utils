8bb13ec973e386dd244024eaa129bd233c31b99a
Duc Trung NGUYEN <ductrung.nguyen@amadeus.com>
2023-02-14 12:00:51 +0100

    feat: add file watcher

---
+++ pkg/filewatcher/filewatcher.go
@@ -0,0 +1,145 @@
+package filewatcher
+
+import (
+	"context"
+	"sync"
+	"time"
+
+	"github.com/ductrung-nguyen/goapp-utils/pkg/utils"
+	"github.com/fsnotify/fsnotify"
+	"golang.org/x/sys/unix"
+)
+
+// FileWatcher is a helper struct that helps us watching a file
+// event if the file does't exist yet
+type FileWatcher struct {
+	sync.Mutex
+	filePath       string
+	fileExist      bool
+	inWatchedQueue bool
+
+	watcher *fsnotify.Watcher
+
+	onChangeHandler func(f *FileWatcher, event fsnotify.Event)
+	onErrorHandler  func(f *FileWatcher, err error) bool
+
+	// the interval that we will check the existence of the file again
+	// default = 5s, can be adjustable, especially for unittests
+	CheckFileInterval time.Duration
+}
+
+// Create new watcher for a file
+// with some pre-defined handlers
+func New(
+	filePath string,
+	onChangeHandler func(f *FileWatcher, event fsnotify.Event),
+	onErrorHandler func(f *FileWatcher, err error) bool,
+) (*FileWatcher, error) {
+	w, err := fsnotify.NewWatcher()
+	if err != nil {
+		return nil, err
+	}
+
+	f := FileWatcher{
+		filePath:  filePath,
+		fileExist: false, inWatchedQueue: false,
+		watcher:           w,
+		onChangeHandler:   onChangeHandler,
+		onErrorHandler:    onErrorHandler,
+		CheckFileInterval: 5 * time.Second,
+	}
+
+	return &f, nil
+}
+
+// the watched file exists ot not?
+func (f *FileWatcher) FileExist() bool {
+	f.Lock()
+	defer f.Unlock()
+	return f.fileExist
+}
+
+// set the existence of the watched file
+func (f *FileWatcher) SetFileExist(value bool) {
+	f.Lock()
+	defer f.Unlock()
+	f.fileExist = value
+}
+
+// is the file in the watched queue?
+func (f *FileWatcher) InWatchedQueue() bool {
+	f.Lock()
+	defer f.Unlock()
+	return f.inWatchedQueue
+}
+
+// update the status of the file in the watched queue
+func (f *FileWatcher) SetInWatchedQueue(value bool) {
+	f.Lock()
+	defer f.Unlock()
+	f.inWatchedQueue = value
+}
+
+// Start watching the file
+// Even when the file is deleted and re-created, the watcher can still put an eye on it
+// until the context is done
+func (f *FileWatcher) Watch(ctx context.Context) {
+	if !f.FileExist() && utils.FileExists(f.filePath) {
+		f.SetFileExist(true)
+	}
+
+	done := make(chan bool)
+	go func() {
+		defer close(done)
+		for {
+			select {
+			case event, ok := <-f.watcher.Events:
+				if !ok {
+					return
+				}
+				if event.Op == fsnotify.Create {
+					f.SetFileExist(true)
+				} else if event.Op == fsnotify.Remove {
+					f.SetFileExist(false)
+					f.SetInWatchedQueue(false)
+				}
+				if f.onChangeHandler != nil {
+					f.onChangeHandler(f, event)
+				}
+			case err, ok := <-f.watcher.Errors:
+				if !ok {
+					return
+				}
+				if f.onErrorHandler != nil {
+					f.onErrorHandler(f, err)
+				}
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+
+	go func() {
+		for {
+			if !f.FileExist() && utils.FileExists(f.filePath) {
+				f.SetFileExist(true)
+				if f.onChangeHandler != nil {
+					f.onChangeHandler(f, fsnotify.Event{Name: f.filePath, Op: fsnotify.Create})
+				}
+			}
+			if f.FileExist() && !f.InWatchedQueue() {
+				if err := f.watcher.Add(f.filePath); err != nil {
+					if err == unix.ENOENT {
+						f.SetFileExist(false)
+					}
+				} else {
+					f.SetInWatchedQueue(true)
+				}
+			}
+
+			time.Sleep(f.CheckFileInterval)
+		}
+	}()
+
+	<-done
+}
---
+++ pkg/filewatcher/filewatcher_test.go
@@ -0,0 +1,114 @@
+package filewatcher
+
+import (
+	"io/fs"
+	"io/ioutil"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/ductrung-nguyen/goapp-utils/pkg/logger"
+	"github.com/ductrung-nguyen/goapp-utils/pkg/utils"
+	"github.com/fsnotify/fsnotify"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"golang.org/x/net/context"
+)
+
+func TestFileWatcher(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "VCFlag test suite")
+}
+
+var _ = Describe("Test filewatcher", func() {
+
+	var watchedFile string
+	BeforeEach(func() {
+		logger.InitLogger(nil)
+		f, err := ioutil.TempFile("/tmp", "test-watcher")
+		if err != nil {
+			PanicWith(err)
+		}
+		if err := f.Close(); err != nil {
+			PanicWith(err)
+		}
+
+		watchedFile = f.Name()
+	})
+
+	AfterEach(func() {
+		if utils.FileExists(watchedFile) {
+			os.Remove(watchedFile)
+		}
+	})
+
+	When("file exists already, and then removed, and added again", func() {
+		It("should detect all changes", func() {
+			events := []fsnotify.Event{}
+
+			ioutil.WriteFile(watchedFile, []byte("This is a dummy file"), fs.ModePerm)
+
+			fw, err := New(watchedFile, func(f *FileWatcher, event fsnotify.Event) {
+				events = append(events, event)
+			}, nil)
+
+			Expect(err).ShouldNot(HaveOccurred())
+			Expect(fw).ToNot(BeNil())
+
+			fw.CheckFileInterval = 5 * time.Millisecond
+
+			ctx, cancel := context.WithCancel(context.TODO())
+
+			// start the watcher
+			go fw.Watch(ctx)
+
+			// start doing the change on the file in another go-routine
+			go func() {
+				// wait a bit for the watcher to detect the file
+				time.Sleep(10 * time.Millisecond)
+
+				os.Remove(watchedFile)
+				time.Sleep(10 * time.Millisecond)
+
+				// note that this action only triggers the event CREATE
+				// the event WRITE is paused until another event raised
+				if err := ioutil.WriteFile(watchedFile, []byte("Trigger the action CREATE + WRITE"), fs.ModePerm); err != nil {
+					PanicWith(err)
+				}
+				time.Sleep(10 * time.Millisecond)
+
+				if err := ioutil.WriteFile(watchedFile, []byte("Execute action WRITE"), fs.ModePerm); err != nil {
+					PanicWith(err)
+				}
+				time.Sleep(10 * time.Millisecond)
+
+				// remove the file again, then create again
+				os.Remove(watchedFile)
+				time.Sleep(10 * time.Millisecond)
+
+				// note that this action only triggers the event CREATE
+				// the event WRITE is paused until another event raised
+				if err := ioutil.WriteFile(watchedFile, []byte("Removed and created again"), fs.ModePerm); err != nil {
+					PanicWith(err)
+				}
+				time.Sleep(10 * time.Millisecond)
+			}()
+
+			time.Sleep(400 * time.Millisecond)
+
+			cancel()
+
+			Expect(events).To(Equal([]fsnotify.Event{
+				{Name: watchedFile, Op: fsnotify.Remove},
+				{Name: watchedFile, Op: fsnotify.Create},
+				{Name: watchedFile, Op: fsnotify.Write},
+				{Name: watchedFile, Op: fsnotify.Write},
+
+				{Name: watchedFile, Op: fsnotify.Remove},
+				{Name: watchedFile, Op: fsnotify.Create},
+				// {Name: watchedFile, Op: fsnotify.Write},
+			}))
+
+		})
+	})
+})
